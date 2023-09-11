b345cdbe33805705708ecb7e3828ccc323d0bccb
Alphonse Johnson <a.joh26@gmail.com>
2023-09-11 14:17:50 -0500

    added services pages/ components/ services requests

---
+++ src/pages/ServiceRequests.js
@@ -0,0 +1,15 @@
+
+import {Form } from '../components/ServiceRequests';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+        <Form />
+      
+    </div>
+  );
+}
+
+export default App;
