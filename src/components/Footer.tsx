5565abd9d0d994190c89c9453705b1b612a65470
unknown <thomasjjohnson9311@gmail.com>
2023-10-08 12:51:53 -0500

    footer stuff

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,16 @@ import Link from "next/link"
 
 function Footer() {
   return (
-      <footer className="flex justify-center gap-4 p-5">
-          {/* Testing self-end with this div */}
+    <footer className="flex justify-center gap-4 p-5">
+      {/* Testing self-end with this div */}
       <Link href="mailto:JDELTA@gmail.com">
         <button>
           <span>JDELTA@gmail.com</span>
         </button>
       </Link>
-          <div className="flex self-end gap-4"> 
-          <span>Phone: 318-555-5555</span>
-              </div>
+      <div className="flex self-end gap-4">
+        <span>Phone: 318-555-5555</span>
+      </div>
     </footer>
   )
 }
