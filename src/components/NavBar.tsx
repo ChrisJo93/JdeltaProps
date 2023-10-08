9deb95968cc0def4532ba59c3be1b80c37b450e3
unknown <thomasjjohnson9311@gmail.com>
2023-10-07 20:59:57 -0500

    adjusting space between nav items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,28 @@
 import React from "react"
 import Link from "next/link"
 
-
 function NavBar() {
   return (
-    <div className="flex justify-center gap-3">
+    <div className="flex justify-center gap-10">
       <Link href="/services">
-      <button>
-        <span className="font-bold">Services</span>
+        <button>
+          <span className="font-bold">Services</span>
         </button>
-        </Link>
+      </Link>
       <Link href="/listings">
-      <button>
-        <span className="font-bold">Listings</span>
-      </button>
+        <button>
+          <span className="font-bold">Listings</span>
+        </button>
       </Link>
-        <Link href="/properties">
-      <button>
-        <span className="font-bold">Properties</span>
-      </button>
+      <Link href="/properties">
+        <button>
+          <span className="font-bold">Properties</span>
+        </button>
       </Link>
-        <Link href="#">
-      <button>
-        <span className="font-bold">NavLink4</span>
-      </button>
+      <Link href="#">
+        <button>
+          <span className="font-bold">NavLink4</span>
+        </button>
       </Link>
     </div>
   )
