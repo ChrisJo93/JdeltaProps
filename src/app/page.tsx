e747d4a330b2f29618e4a8d2a01671a8ab12d4e1
Chris Johnson <Johnny.C.Alexander@gmail.com>
2023-12-30 21:01:28 -0600

    adding about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
-// import Link from "next/link"
 import React from "react"
-// import NavBar from "../components/NavBar"
 import Header from "@/components/Header"
+import About from "@/components/About"
+import Banner from "@/components/Banner"
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <Header />
       <section>
-        <div
-          className="grid bg-cover bg-center bg-no-repeat min-h-screen min-w-full "
-          style={{
-            backgroundImage: `url("https://images.unsplash.com/photo-1589577428135-ff35b724950b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGxvdWlzaWFuYXxlbnwwfHwwfHx8MA%3D%3D")`,
-          }}
-        >
-          <div className="text-7xl absolute bottom-0 left-0 text-rose-100	">
-            <p className="max-w-screen-sm">Turning keys into Comfort</p>
-          </div>
-        </div>
+        <Banner />
       </section>
-      <section>about us</section>
+      <section>
+        <About />
+      </section>
       <section id="listings">current listings</section>
       <section>services</section>
       <section>contact info</section>
---
+++ src/components/About.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div
+      id="about_us"
+      className="grid align-center justify-center bg-cover bg-center bg-no-repeat min-h-screen min-w-full bg-white text-slate-700 text-3xl"
+    >
+      <div className="text-center	leading-normal max-w-auto ">
+        <p className="justify-self-center mt-32 text-4xl mb-3">About Us</p>
+        <p className="mx-52 text-xl text-balance">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui sapien
+          eget mi proin sed libero enim sed faucibus. Orci dapibus ultrices in
+          iaculis nunc. Quisque id diam vel quam elementum pulvinar etiam non
+          quam. Ornare aenean euismod elementum nisi quis eleifend. Felis
+          bibendum ut tristique et egestas quis ipsum. Adipiscing elit
+          pellentesque habitant morbi tristique. Vitae et leo duis ut. Nec nam
+          aliquam sem et tortor consequat id porta nibh. Urna id volutpat lacus
+          laoreet non curabitur gravida arcu ac. Potenti nullam ac tortor vitae
+          purus. Sit amet justo donec enim diam vulputate ut. Pharetra convallis
+          posuere morbi leo urna molestie at.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
