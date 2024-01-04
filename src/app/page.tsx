8fe9b182cd63e97de8a5939a9e94351f069de174
unknown <thomasjjohnson9311@gmail.com>
2024-01-03 23:18:10 -0600

    implemented footer w/ dummy info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       </section>
       <section>
         <Footer />
-     </section>
+      </section>
     </main>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,27 @@ import Link from "next/link"
 
 function Footer() {
   return (
-    <footer className="flex justify-center gap-4 p-5">
+    <footer className="grid grid-cols-2 justify-center gap-4 p-5">
       {/* Testing self-end with this div */}
+      <div>
+        <p className="text-3xl">
+          JDELTA Properties
+        </p>
+      </div>
+      <div>
+        <p className="text-2xl">Address</p>
+        <p className="text-sm">1111 JDELTA Drive</p>
+        <p className="text-sm">Shreveport, Louisiana</p>
+        <p className="text-sm">71101</p>
+      </div>
       <Link href="mailto:JDELTA@gmail.com">
         <button>
-          <span>JDELTA@gmail.com</span>
+          <span className="text-2xl">JDELTA@gmail.com</span>
         </button>
       </Link>
-      <div className="flex self-end gap-4">
-        <span>Phone: 318-555-5555</span>
+      <div>
+        <span>Phone Number:</span>
+        <p className="text-sm">318-555-5555</p>
       </div>
     </footer>
   )
